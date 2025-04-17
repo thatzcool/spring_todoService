917df369ff7110b093c164343a82c82eddeff076
thatzcool <mycaptain0771@gmail.com>
2025-04-17 14:19:54 +0900

    spring_todo_todolist_start

--- src/main/java/com/ssg/todoservice/mapper/TodoMapper.java
+++ src/main/java/com/ssg/todoservice/mapper/TodoMapper.java
@@ -2,6 +2,9 @@ package com.ssg.todoservice.mapper;
 
 import com.ssg.todoservice.domain.TodoVO;
 
+import java.util.List;
+
 public interface TodoMapper {
     void insert(TodoVO todoVO);
+    List<TodoVO> selectAll();
 }
--- src/test/java/com/ssg/todoservice/mapper/TodoMapperTests.java
+++ src/test/java/com/ssg/todoservice/mapper/TodoMapperTests.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -27,5 +28,11 @@ public class TodoMapperTests {
                  .build();
          todoMapper.insert(todoVO);
 
+    }
+
+    @Test
+    public void testSelectAll() {
+        List<TodoVO> voList = todoMapper.selectAll();
+        voList.forEach(System.out::println);
     }
 }
